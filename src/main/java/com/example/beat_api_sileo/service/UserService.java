7b9c613d5a2cd7f94996bfe2593647e226d3455f
sileo18 <lucastoledo358@gmail.com>
2025-04-27 01:45:05 -0300

    feat: adding login service

--- src/main/java/com/example/beat_api_sileo/service/UserService.java
+++ src/main/java/com/example/beat_api_sileo/service/UserService.java
@@ -1,20 +1,29 @@
 package com.example.beat_api_sileo.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.example.beat_api_sileo.config.TokenService;
+import com.example.beat_api_sileo.domain.Api.LoginRequestDTO;
+import com.example.beat_api_sileo.domain.Api.LoginResponseDTO;
+import com.example.beat_api_sileo.domain.Api.RegisterResponseDTO;
+import com.example.beat_api_sileo.domain.Roles.RoleEnum;
+import com.example.beat_api_sileo.domain.Roles.Roles;
 import com.example.beat_api_sileo.domain.User.UserRegisterDTO;
 import com.example.beat_api_sileo.domain.User.User;
+import com.example.beat_api_sileo.domain.UserRole.UserRole;
+import com.example.beat_api_sileo.exceptions.EmailAlreadyExists;
+import com.example.beat_api_sileo.exceptions.InvalidCredentials;
+import com.example.beat_api_sileo.exceptions.UserNotFound;
+import com.example.beat_api_sileo.repositories.RolesRepository;
 import com.example.beat_api_sileo.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Date;
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Service
 public class UserService {
@@ -22,56 +31,79 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Value("${aws.bucket.name}")
-    private String bucketName;
+    @Autowired
+    private RolesRepository rolesRepository;
 
     @Autowired
-    private AmazonS3 amazonS3;
+    private S3Service amazonS3;
 
-    public User createUser(UserRegisterDTO data) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Transactional
+    public RegisterResponseDTO createUser(UserRegisterDTO data) {
 
         String imgUrl = "";
 
-        if(data.getProfilePictureUrl() != null) {
-            imgUrl = this.uploadImg(data.getProfilePictureUrl());
+        if (userRepository.existsByEmail(data.getEmail())) {
+            throw new EmailAlreadyExists("Email already exists");
         }
+
+        if(data.getProfilePictureUrl() != null) {
+            imgUrl = amazonS3.uploadFile(data.getProfilePictureUrl());
+        }
+
+        String encodedPassword = passwordEncoder.encode(data.getPassword());
+
 
         User user = new User();
         user.setName(data.getName());
         user.setEmail(data.getEmail());
-        user.setPassword(data.getPassword());
+        user.setPassword(encodedPassword);
         user.setSurname(data.getSurname());
         user.setDescription(data.getDescription());
         user.setBirthDate(new Date(data.getBirthDate()));
         user.setProfilePictureUrl(imgUrl);
 
+        Roles role = rolesRepository.findByName(RoleEnum.ADMIN)
+                .orElseThrow(() -> new RuntimeException("Role não encontrada: " + RoleEnum.ADMIN));
+
+        Set<Roles> roles = new HashSet<>();
+        roles.add(role);
+        user.setRoles(roles);
+
         userRepository.save(user);
 
-        return user;
+        String token = tokenService.generateToken(user);
 
+        return new RegisterResponseDTO(user, token);
     }
 
-    private String uploadImg(MultipartFile multipartFile) {
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
 
-        String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        String email = loginRequest.getEmail().trim();
+        String password = loginRequest.getPassword();
 
-        try {
-            File file = this.convertMultiPartToFile(multipartFile);
-            amazonS3.putObject(bucketName,fileName, file);
-            file.delete();
-            return amazonS3.getUrl(bucketName, fileName).toString();
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + "Error uploading image");
-            e.printStackTrace();
-            return null;
+        System.out.println("Attempting to login with email: " + email);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFound("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new InvalidCredentials("Invalid credentials");
         }
+
+        String token = tokenService.generateToken(user);
+
+        return new LoginResponseDTO(token, user.getName(), user.getId());
     }
 
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
+
 }
